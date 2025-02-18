f1ec063c223753cd0c45daa361901daede9e1f3d
rohanp774 <roshpan12@gmail.com>
2025-02-18 20:34:25 +0000

    setup project for worldgen

---
+++ src/main/java/net/thetruebeep/datagen/ModDatapackProvider.java
@@ -0,0 +1,27 @@
+package net.thetruebeep.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+import net.thetruebeep.beepsenvironmentalmod.BeepsEnvironmentalMod;
+import net.thetruebeep.beepsenvironmentalmod.worldgen.ModBiomeModifiers;
+import net.thetruebeep.beepsenvironmentalmod.worldgen.ModConfiguredFeatures;
+import net.thetruebeep.beepsenvironmentalmod.worldgen.ModPlacedFeatures;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
+
+    public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Set.of(BeepsEnvironmentalMod.MOD_ID));
+    }
+}
