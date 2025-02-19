e69696a7a3c9b8f6c5892fdfce3c292c08e62c99
rohanp774 <roshpan12@gmail.com>
2025-02-19 20:04:17 +0000

    terrain gen for rosewood

--- src/main/java/net/thetruebeep/beepsenvironmentalmod/worldgen/ModBiomeModifiers.java
+++ src/main/java/net/thetruebeep/beepsenvironmentalmod/worldgen/ModBiomeModifiers.java
@@ -6,6 +6,7 @@ import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.neoforged.neoforge.common.world.BiomeModifier;
 import net.neoforged.neoforge.common.world.BiomeModifiers;
@@ -14,6 +15,8 @@ import net.thetruebeep.beepsenvironmentalmod.BeepsEnvironmentalMod;
 
 public class ModBiomeModifiers {
     public static final ResourceKey<BiomeModifier> ADD_CHROMIUM_ORE = registerKey("add_chromium_ore");
+
+    public static final ResourceKey<BiomeModifier> ADD_TREE_ROSE_WOOD = registerKey("add_tree_rose_wood");
 
 
     public static void bootstrap(BootstrapContext<BiomeModifier> context) {
@@ -25,6 +28,11 @@ public class ModBiomeModifiers {
                 HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.CHROMIUM_ORE_PLACED_KEY)),
                 GenerationStep.Decoration.UNDERGROUND_ORES
         ));
+
+        context.register(ADD_TREE_ROSE_WOOD, new BiomeModifiers.AddFeaturesBiomeModifier(
+                HolderSet.direct(biomes.getOrThrow(Biomes.SAVANNA)),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.ROSE_WOOD_PLACED_KEY)),
+                GenerationStep.Decoration.VEGETAL_DECORATION));
 
     }
 
--- src/main/java/net/thetruebeep/beepsenvironmentalmod/worldgen/ModPlacedFeatures.java
+++ src/main/java/net/thetruebeep/beepsenvironmentalmod/worldgen/ModPlacedFeatures.java
@@ -3,6 +3,8 @@ package net.thetruebeep.beepsenvironmentalmod.worldgen;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -11,17 +13,23 @@ import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 import net.thetruebeep.beepsenvironmentalmod.BeepsEnvironmentalMod;
+import net.thetruebeep.beepsenvironmentalmod.block.ModBlocks;
 
 import java.util.List;
 
 public class ModPlacedFeatures {
     public static final ResourceKey<PlacedFeature> CHROMIUM_ORE_PLACED_KEY = registerKey("chromium_ore_placed");
+    public static final ResourceKey<PlacedFeature> ROSE_WOOD_PLACED_KEY = registerKey("rose_wood_placed");
 
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
         register(context, CHROMIUM_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_CHROMIUM_KEY),
                 ModOrePlacement.commonOrePlacement(7, HeightRangePlacement.uniform(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(5))));
+
+        register(context, ROSE_WOOD_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.ROSE_WOOD_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(1, 0.5f, 1),
+                        ModBlocks.ROSE_WOOD_SAPLING.get()));
     }
 
     private static ResourceKey<PlacedFeature> registerKey(String name) {
